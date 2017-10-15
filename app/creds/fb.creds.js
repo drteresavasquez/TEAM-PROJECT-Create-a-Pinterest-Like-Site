c3efd35b05330b0c2509b85bc5781223701a0d0f
Dr. Teresa Vasquez <capewearingmama@Drs-MacBook-Pro.local>
2017-10-14 19:04:53 -0500

    stash

---
+++ app/creds/fb.creds.js
@@ -0,0 +1,10 @@
+"use strict";
+// console.log("FB Creds Yo!");
+
+//GET THIS FROM FIREBASE OVERVIEW >> Add Firebase to your web app
+app.constant("FBCreds", {
+    apiKey: "AIzaSyDJNiGxj9qIDNwRNpfhf4S1q4ckq0rE330",
+    authDomain: "class-projects-fb145.firebaseapp.com",
+    databaseURL: "https://class-projects-fb145.firebaseio.com"
+});
+
